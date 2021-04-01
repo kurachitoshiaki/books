1171ab79599f37c07bfa8578d7e12f35b31d97ce
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-04-02 02:37:49 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+    @Autowired
+    EmployeeRepository empRepository;
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        List<Employee> emplist=empRepository.findAll();
+        model.addAttribute("emplist", emplist);
+        return "index";
+    }
+}

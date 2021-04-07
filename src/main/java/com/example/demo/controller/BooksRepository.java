a1ef65031f4c0a7165ddf2ba2efb52d698cbad39
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-04-07 23:56:18 +0900

    Delete BooksRepository.java

--- src/main/java/com/example/demo/controller/BooksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.controller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BooksRepository extends JpaRepository<Books, Long> {}

6fb35441371eace4d14740c64906521692fdcf0d
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-04-07 23:56:16 +0900

    Create Book.java

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,49 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="books")
+public class Book {
+    @Id
+    @Column(name="id")
+    private Long id;
+    private String name;
+    private Integer status;
+    private String created_at;
+    private String updated_at;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getStatus() {
+        return status;
+    }
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+    public String getCreated_at() {
+        return created_at;
+    }
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+    public String getUpdated_at() {
+        return updated_at;
+    }
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+}

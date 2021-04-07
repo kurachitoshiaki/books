8cfdf934fa87f4c9468cb95395192da4979b87ad
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-04-07 22:49:43 +0900

    Create BooksRepository.java

---
+++ src/main/java/com/example/demo/controller/BooksRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Books, Long> {}

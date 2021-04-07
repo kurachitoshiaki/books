cadcc9ba6d0fccbe7018d16b4011822c41e4b64d
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-04-07 23:56:05 +0900

    Update BookController.java

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,17 +5,29 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
+
 @Controller
 public class BookController {
-    @Autowired
-    BooksRepository bookRepository;
+	@Autowired
+	BookService bookService;
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
-        List<Books> booklist=bookRepository.findAll();
+        List<Book> booklist=bookService.searchAll();
         model.addAttribute("booklist", booklist);
         return "index";
+    }
+    
+    @GetMapping("/book/{id}")
+    public String displayView(@PathVariable Long id, Model model) {
+      Book book = bookService.findById(id);
+      model.addAttribute("book", book);
+      return "show";
     }
 }

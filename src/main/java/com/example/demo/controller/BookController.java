cc89280fa2378d905e846d400671ad6bd4f28c09
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-04-08 20:26:25 +0900

    Update BookController.java

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.example.demo.entity.Book;
 import com.example.demo.service.BookService;
 

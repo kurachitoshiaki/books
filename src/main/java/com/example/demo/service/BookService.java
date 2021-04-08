e0daa604812323600422af565d03f1fde54ae38e
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-04-08 20:26:29 +0900

    Update BookService.java

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
+
 import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Book;
 import com.example.demo.repository.BookRepository;
 /**
